2253208bcaadbc60ea16b635b83c7329cb1151ff
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-20 12:59:21 +0300

    first master commit

---
+++ UnitTest05.2/UnitTest05.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR05.2/PR05.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest052
+{
+	TEST_CLASS(UnitTest052)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+						int t;
+						t = A(2, 3, 1);
+						Assert::AreEqual(t, 0, 0.00001);
+		}
+	};
+}
